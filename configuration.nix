d0f640d3f5bf7d1bdb84c770d957ee76d077a85d
Francisco J. Rodríguez Martín <pako.rodriguez@gmail.com>
2024-06-01 08:08:44 +0100

    Borrado de algunos comentarios.

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
   };
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
