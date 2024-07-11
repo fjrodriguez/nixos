0fbf5bb2fe6cb17172822a3afae17b9e5f75ff47
Francisco J. Rodríguez Martín <pako.rodriguez@gmail.com>
2024-07-11 21:40:50 +0100

    Added japanese fonts and input.

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -75,12 +75,76 @@
     LC_TIME = "es_ES.UTF-8";
   };
 
+  #############################################################
+  ## Select internationalisation properties for the console. ##
+  #############################################################
+
+  # The font to be used on the console when logging in.  Since most people
+  # only use Japanese in X, you can leave this as the default value.
+  i18n.consoleFont = "Lat2-Terminus16";
+
+  ###################
+  ## Font Settings ##
+  ###################
+
+  # Enable fonts to use on your system.  You should make sure to add at least
+  # one English font (like dejavu_fonts), as well as Japanese fonts like
+  # "ipafont" and "kochi-substitute".
+  fonts.fonts = with pkgs; [
+    carlito
+    dejavu_fonts
+    ipafont
+    kochi-substitute
+    source-code-pro
+    ttf_bitstream_vera
+  ];
+
+  # Enable the "ultimate" font config.  This enables a few extra options to
+  # make sure fonts look nice.  However, if you enable this and fonts look
+  # strange, try disabling it.
+  # fonts.fontconfig.ultimate.enable = true;
+
+  # These settings enable default fonts for your system.  This setting is very
+  # important.  It lets fontconfig know that you want to fall back to a Japanese
+  # font (for example "IPAGothic") if an application tries to show fonts with
+  # Japanese.  For instance, this is important if you are using a terminal
+  # emulator and you `cat` some Japanese text to the screen. If you don't have
+  # "defaultFonts" configured, fontconfig will pick a random Japanese font to
+  # use.  If you have this "defaultFonts" setting configured, fontconfig will
+  # pick the font you have selected.  This makes sure Japanese fonts look nice.
+  fonts.fontconfig.defaultFonts = {
+    monospace = [
+      "DejaVu Sans Mono"
+      "IPAGothic"
+    ];
+    sansSerif = [
+      "DejaVu Sans"
+      "IPAPGothic"
+    ];
+    serif = [
+      "DejaVu Serif"
+      "IPAPMincho"
+    ];
+  };
+
+  ###############################
+  ## Input Method Editor (IME) ##
+  ###############################
+
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [ mozc ];
+  };
+  
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.deviceSection = lib.mkDefault ''
+    Option "TearFree" "true"
+  '';
 
   # Configure keymap in X11
   services.xserver = {
@@ -145,6 +209,8 @@
   podman-tui # status of containers in the terminal
   docker-compose # start group of containers for dev
   podman-compose # start group of containers for dev
+  gnome.polari
+  neovim
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
